78a52e0f2573ffac4cf7aeb447303a4ad470a291
Mudit <muditchaudhary90@gmail.com>
2026-04-20 15:00:47 +0530

    Initial Commit

---
+++ FootPrint/FootPrintApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootPrintApp.swift
+//  FootPrint
+//
+//  Created by Mudit Chaudhary on 20/04/26.
+//
+
+import SwiftUI
+
+@main
+struct FootPrintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
